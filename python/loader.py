6921339fe9001f675283c071e54df8007291c78a
artem <artem10halushkin@gmail.com>
2026-08-20 17:21:03 +0200

    changes

---
+++ python/loader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import kagglehub
+import os
+
+
+path = kagglehub.dataset_download("anuragupadhyaya/anticancer-peptides-data-set")
+
+csv_file_lungs = os.path.join(path, "ACPs_Lung_cancer.csv")
+cvs_file_breast = os.path.join(path, "ACPs_Breast_cancer.csv")
+
+file_lungs = pd.read_csv(csv_file_lungs)
+file_breast = pd.read_csv(cvs_file_breast)
+
+
+eng = create_engine("postgressql//admin:secret_code@db-5432/peptide_db")
+
+file_lungs.to_sql(name="peptides_l", con=eng , if_exists="replace" , index = False)
+file_breast.to_sql(name="peptides_b",con=eng , if_exists="replace", index = False)
+
+
+
