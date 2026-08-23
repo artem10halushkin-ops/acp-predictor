8240fe82bb1d49a2ae6bbfb47efd71eddf7e6607
artem <artem10halushkin@gmail.com>
2026-08-23 21:53:55 +0200

    time before DataLoader..

--- python/trainer.py
+++ python/trainer.py
@@ -1,9 +1,11 @@
 import torch
+from sqlalchemy.dialects.oracle import dictionary
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 import pandas as pd
 import psycopg2 as ps
 from sqlalchemy import create_engine
+max_length = 20
 
 eng = create_engine("postgresql://admin:secret_code@localhost:5432/peptide_db")
 
@@ -32,22 +34,53 @@ def get_our_db_breast():
     return df
 
 
-translation = {
-    'padding': 0, 'A': 1,
-    'W': 2, 'K': 3, 'E': 4,
-    'F': 5, 'I': 6, 'T': 7,
-    'C': 8, 'D': 9, 'L': 10,
-    'M': 11, 'N': 12, 'P': 13,
-    'Q': 14, 'R': 15, 'S': 16,
-    'G': 17, 'V': 18, 'H': 19, 'Y': 20,
-}
 
-class translator:
-    def pad_adjusting(self, n:int, sequence:list ):
+
+translation = {
+        'padding': 0, 'A': 1,
+        'W': 2, 'K': 3, 'E': 4,
+        'F': 5, 'I': 6, 'T': 7,
+        'C': 8, 'D': 9, 'L': 10,
+        'M': 11, 'N': 12, 'P': 13,
+        'Q': 14, 'R': 15, 'S': 16,
+        'G': 17, 'V': 18, 'H': 19, 'Y': 20,
+ }
+
+
+
+class PeptideDataSet(Dataset):
+
+    def padd_adjusting(self, n: int, sequence: list):
         pad = torch.zeros(n, dtype=torch.long)
-        tensor_seq = torch.tensor(sequence , dtype= torch.long)
+        tensor_seq = torch.tensor(sequence[:n], dtype=torch.long)
         pad[:len(tensor_seq)] = tensor_seq
         return pad
+
+    def __init__(self, df , trans_dict ,max_length):
+        self.dataframe = df
+        self.m_Length = max_length
+        self.dictionary = trans_dict
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        row = self.dataframe.iloc[index]
+        seq = row['sequence']
+        affect = row['class']
+        tmp_list = []
+        for letter in seq:
+            tmp_list.append( self.dictionary.get(letter,0) )
+        tmp_list = self.padd_adjusting( self.m_Length, tmp_list )
+
+        target_tensor = torch.tensor(affect , dtype= torch.float32)
+        return tmp_list , target_tensor
+
+
+
+
+
+
 
 
 
